35915ec1b7bf22d62729698c24e7cf500520b7f6
Endra Janitra <janitraendra@gmail.com>
2026-03-09 10:54:46 +0700

    fix label & navigation grup list biro

--- app/Filament/Resources/ListBiros/ListBiroResource.php
+++ app/Filament/Resources/ListBiros/ListBiroResource.php
@@ -11,6 +11,7 @@ use App\Filament\Resources\ListBiros\Schemas\ListBiroInfolist;
 use App\Filament\Resources\ListBiros\Tables\ListBirosTable;
 use App\Models\ListBiro;
 use BackedEnum;
+use UnitEnum;
 use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
@@ -19,8 +20,12 @@ use Filament\Tables\Table;
 class ListBiroResource extends Resource
 {
     protected static ?string $model = ListBiro::class;
+    protected static string | UnitEnum | null $navigationGroup= 'Surat Keluar';
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static ?string $navigationLabel = 'List Biro';
+
+
+    #protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
 
     protected static ?string $recordTitleAttribute = 'list_biro';
 
