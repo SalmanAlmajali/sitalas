04929b7f8c00594bde9589440ef95734c55b41b6
Endra Janitra <janitraendra@gmail.com>
2026-03-18 14:52:05 +0700

    feat: hak akses list biro

---
+++ app/Policies/ListBiroPolicy.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\ListBiro;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class ListBiroPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->isAdmin() || $user->isUser();
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, ListBiro $listBiro): bool
+    {
+        return $user->isAdmin() || $user->isUser();
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->isAdmin() || $user->isUser();
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, ListBiro $listBiro): bool
+    {
+        return $user->isAdmin() || $user->isUser();
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, ListBiro $listBiro): bool
+    {
+        return $user->isAdmin() || $user->isUser();
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, ListBiro $listBiro): bool
+    {
+        return $user->isAdmin() || $user->isUser();
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, ListBiro $listBiro): bool
+    {
+        return $user->isAdmin() || $user->isUser();
+    }
+}
