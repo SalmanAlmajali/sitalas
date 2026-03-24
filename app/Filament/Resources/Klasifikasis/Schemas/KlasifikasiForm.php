837b1e69dfbf6552aebb6fbda7b3ecfde1925913
Draa7 <janitraendra@gmail.com>
2026-03-24 22:28:46 +0700

    fix: tampilan form klasifikasi surat

--- app/Filament/Resources/Klasifikasis/Schemas/KlasifikasiForm.php
+++ app/Filament/Resources/Klasifikasis/Schemas/KlasifikasiForm.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Resources\Klasifikasis\Schemas;
 
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Grid;
 use Filament\Schemas\Schema;
 
 class KlasifikasiForm
@@ -11,9 +13,20 @@ class KlasifikasiForm
     {
         return $schema
             ->components([
-                TextInput::make('klasifikasi')
-                    ->label('klasifikasi Surat')
-                    ->required(),
+
+                Section::make('Informasi Klasifikasi Surat')
+                    ->description('Masukkan jenis klasifikasi surat')
+                    ->schema([
+                        Grid::make(1)->schema([
+                            TextInput::make('klasifikasi')
+                                ->label('Klasifikasi Surat')
+                                ->placeholder('Contoh: Undangan / Laporan / Internal')
+                                ->required()
+                                ->unique(ignoreRecord: true)
+                                ->maxLength(100),
+                        ]),
+                    ])
+                    ->columnSpanFull(),
             ]);
     }
 }
