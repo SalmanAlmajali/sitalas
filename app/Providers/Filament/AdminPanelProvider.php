a8a5791e06322c187f203e9c7cf3d431e9e18290
Endra Janitra <janitraendra@gmail.com>
2026-03-24 16:00:30 +0700

    feat: brandname web

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,6 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->brandName('SITALAS') 
             ->colors([
                 'primary' => Color::Amber,
             ])
